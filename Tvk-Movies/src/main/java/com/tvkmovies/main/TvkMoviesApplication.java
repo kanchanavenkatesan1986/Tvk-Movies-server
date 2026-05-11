62da0ddf191e65c670c982188d750cc841f870ac
allanimetamil <teenteenagepassanga@gmail.com>
2026-05-12 02:41:13 +0530

    begin

---
+++ Tvk-Movies/src/main/java/com/tvkmovies/main/TvkMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.tvkmovies.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvkMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvkMoviesApplication.class, args);
+	}
+
+}
